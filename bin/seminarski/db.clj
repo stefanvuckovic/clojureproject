6614c9bc789582e0d76a5abc222d4cb496b3db99
Stefan Vuckovic <stefan@stefanvuckovic.com>
2015-08-23 11:23:20 +0200

    Added connection and communication with mongodb

---
+++ bin/seminarski/db.clj
@@ -0,0 +1,27 @@
+(ns seminarski.db
+  
+  (:require [monger.core :as mo]))
+  
+
+(def connection (mg/connect {:host "127.0.0.1" :port 27017}))
+
+(def db (mg/get-db connection "database"))
+
+(defn insert [col doc] 
+  (mo/insert db col doc)                
+)    
+
+(defn insertbatch [col docs] 
+  (mo/insert-batch db col docs)                
+)  
+  
+(defn select [col condition] 
+  (if(nil? condition)
+    (mo/find-maps db col) 
+    (mo/find-maps db col condition))                               
+)
+ 
+(defn isempty [col] 
+  (mo/empty? db col)
+)
+
