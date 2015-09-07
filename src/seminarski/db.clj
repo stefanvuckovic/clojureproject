f9766264fff2b7e1102884598555e83a73d7f84e
Stefan Vuckovic <stefan@stefanvuckovic.com>
2015-09-07 21:32:24 +0200

    Fixed communication with mongodb database

--- src/seminarski/db.clj
+++ src/seminarski/db.clj
@@ -1,7 +1,9 @@
 (ns seminarski.db
   
   (:require [monger.core :as mo]
-            [monger.collection :as mcol]))
+            [monger.collection :as mcol]
+            [monger.query :as mquery]
+            [monger.operators :refer :all]))
   
 
 (def connection (mo/connect {:host "127.0.0.1" :port 27017}))
@@ -16,10 +18,19 @@
   (mcol/insert-batch db col docs)                
 )  
   
-(defn get-data [col condition] 
-  (if(nil? condition)
-    (mcol/find-maps db col) 
-    (mcol/find-maps db col condition))                               
+(defn get-data [col & {:keys [condition] :or {condition {}}}]
+    (mcol/find-maps db col condition)                               
+)
+
+(defn get-data-pagination [col page limit & {:keys [condition srt] :or {condition {} srt {}}}]
+  (mquery/with-collection db col
+    (mquery/find condition)
+    (mquery/sort srt)
+    (mquery/paginate :page page :per-page limit)) 
+)
+
+(defn delete-data [col & {:keys [condition] :or {condition {}}}]
+  (mcol/remove db col condition)
 )
  
 (defn isempty [col] 
